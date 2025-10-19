fb3b347b8bf539b3a0152448b7c2d2288deb5bc1
Gaspar Csirmaz <csirmazg@gmail.com>
2025-10-19 22:37:11 +0200

    set deafault uri from prot 7000 to empty string

--- src/frontend-app/src/services/api-client-base.ts
+++ src/frontend-app/src/services/api-client-base.ts
@@ -2,9 +2,9 @@ import type { AxiosResponse } from "axios";
 
 export abstract class ApiClientBase {
     protected baseUrl: string | undefined;
-    
+
     protected getBaseUrl(url: string): string {
-        const baseUrl = import.meta.env.VITE_API_BASE_URL || 'https://localhost:7000';
+        const baseUrl = import.meta.env.VITE_API_BASE_URL || '';
         return baseUrl + url;
     }
 
